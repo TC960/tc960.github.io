095e7298e1cc46f8f76acca507565906f634e19e
Mohak Akul Prakash <mohakprakash2016@gmail.com>
2025-10-28 15:49:08 -0700

    add cursor

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import SplashCursor from './components/animations/SplashCursor';
 
 const App = () => {
   return (
     <Router>
+      <SplashCursor
+        SPLAT_RADIUS={0.14}
+        SPLAT_FORCE={4000}
+        CURL={2}
+        DENSITY_DISSIPATION={4.5}
+        VELOCITY_DISSIPATION={3}
+        SHADING={false}
+        COLOR_UPDATE_SPEED={0}
+        CUSTOM_COLOR={{ r: 0.24, g: 0.11, b: 0 }}
+        SIM_RESOLUTION={128}
+        DYE_RESOLUTION={1024}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
